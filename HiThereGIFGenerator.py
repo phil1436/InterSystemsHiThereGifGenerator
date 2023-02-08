9de3ed99dc79d95a4647641c4462e2c26f8a45b6
phil1436 <philxsb@gmail.com>
2023-02-08 16:49:50 +0100

    python module added

--- HiThereGIFGenerator.py
+++ HiThereGIFGenerator.py
@@ -3,44 +3,77 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 
-class GIFPart:
-    def __init__(self, first: str, second: str):
-        if (len(first) + len(second) > 20):
-            raise ValueError(
-                "The length of the first part and the second part must be less than 20 characters combined!")
+class HTGIFPart:
+    def __init__(self, path_to_font_ttf: str, first: str, second: str = None):
         self.first = first
         self.second = second
         self.first_color = (0, 181, 175)
         self.second_color = (51, 54, 149)
+        self.font = path_to_font_ttf
+
+    # change the color of the first part
+    def set_first_color(self, color):
+        self.first_color = color
+
+    # change the color of the second part
+    def set_second_color(self, color):
+        self.second_color = color
+
+    # toString
+    def to_string(self):
+        if (self.second == None):
+            return self.first
+        return self.first + " " + self.second
 
 
-class GIFGenerator:
-    def __init__(self):
+class HTGIFGenerator:
+    def __init__(self, path_to_background_image: str, file_output: str = None, start_gif: str = None, end_gif: str = None):
         self.duration = 80
         self.hold = 15
-        self.max_width = 900
-        self.template_file = "imgs/template.png"
-        self.start_gif = "imgs/start.gif"
-        self.end_gif = "imgs/end.gif"
-        self.file_output = "out/test.gif"
+        self.background = path_to_background_image
+        width, height = Image.open(path_to_background_image).size
+        self.max_width = width
+        self.start_gif = start_gif
+        self.end_gif = end_gif
+        if (file_output == None):
+            self.file_output = "HiThere.gif"
+        else:
+            self.file_output = file_output
         self.parts = []
-        self.font = 'font/DINAlternate-Bold.ttf'
 
-    def set_duration(self, duration):
+    # change the duration of the gif
+    def set_duration(self, duration: int):
         self.duration = duration
 
-    def set_hold(self, hold):
+    # change the hold time
+    def set_hold(self, hold: int):
         self.hold = hold
 
-    def set_file_output_path(self, file_output_path):
-        self.file_output_path = file_output_path
+    # change the output file
+    def set_file_output(self, file_output_path: str):
+        self.file_output = file_output_path
 
-    def add_part(self, part):
+    # change the start gif
+    def set_start_gif(self, start_gif_path: str):
+        self.start_gif = start_gif_path
+
+    # change the end gif
+    def set_end_gif(self, end_gif_path: str):
+        self.end_gif = end_gif_path
+
+    # change the max width of the text
+    def set_max_width(self, max_width: int):
+        self.max_width = max_width
+
+    # add a part to the gif
+    def add_part(self, part: HTGIFPart):
         self.parts.append(part)
 
+    # reset the parts
     def reset(self):
         self.parts = []
 
+    # generate the gif
     def generate(self):
         if (self.parts == []):
             raise ValueError("There should be at least one part!")
@@ -48,9 +81,10 @@ class GIFGenerator:
         frames = []
 
         # add start gif
-        for frame in ImageSequence.Iterator(Image.open(self.start_gif)):
-            frame = frame.copy()
-            frames.append(frame)
+        if (self.start_gif != None):
+            for frame in ImageSequence.Iterator(Image.open(self.start_gif)):
+                frame = frame.copy()
+                frames.append(frame)
 
         # add the parts
         for parts in self.parts:
@@ -67,39 +101,40 @@ class GIFGenerator:
                 frames.append(img)
 
         # add start gif
-        for frame in ImageSequence.Iterator(Image.open(self.end_gif)):
-            frame = frame.copy()
-            frames.append(frame)
+        if (self.end_gif != None):
+            for frame in ImageSequence.Iterator(Image.open(self.end_gif)):
+                frame = frame.copy()
+                frames.append(frame)
 
         frame_one = frames[0]
         frame_one.save(self.file_output, format="GIF", append_images=frames[1:],
                        save_all=True, duration=self.duration, loop=0, disposal=2)
 
-    def generate_images(self, part: GIFPart):
+    # generate the images for a part
+    def generate_images(self, part: HTGIFPart):
 
-        img = Image.open(self.template_file)
+        img = Image.open(self.background)
         I1 = ImageDraw.Draw(img)
 
         # adjust font size
-        myFont = ImageFont.truetype(self.font, 1)
+        myFont = ImageFont.truetype(part.font, 1)
         width, height = img.size
-        size_width = I1.textlength(part.first + " " + part.second, myFont)
+        size_width = I1.textlength(part.to_string(), myFont)
         pt = 1
         # get the maximal font size
         while (size_width < self.max_width - 30):
             pt += 1
-            myFont = ImageFont.truetype(self.font, pt)
-            size_width = I1.textlength(part.first + " " + part.second, myFont)
+            myFont = ImageFont.truetype(part.font, pt)
+            size_width = I1.textlength(part.to_string(), myFont)
 
         images = []
 
         y = height / 2 - \
-            (I1.textbbox((0, 0), part.first + " " +
-             part.second, font=myFont)[3] / 2)
+            (I1.textbbox((0, 0), part.to_string(), font=myFont)[3] / 2)
 
         first = ""
         for char in part.first:
-            img = Image.open(self.template_file)
+            img = Image.open(self.background)
             first += char
             I1 = ImageDraw.Draw(img)
 
@@ -107,7 +142,6 @@ class GIFGenerator:
 
             x = width/2 - (letters_width / 2)
 
-            # add name
             I1.text((x, y), first,
                     fill=part.first_color, font=myFont)
 
@@ -115,9 +149,12 @@ class GIFGenerator:
 
         first_width = I1.textlength(first + " ", myFont)
 
+        if (part.second == None):
+            return images
+
         second = ""
         for char in part.second:
-            img = Image.open(self.template_file)
+            img = Image.open(self.background)
             second += char
             I1 = ImageDraw.Draw(img)
 
@@ -130,7 +167,6 @@ class GIFGenerator:
             I1.text((x, y), first,
                     fill=part.first_color, font=myFont)
 
-            # add name
             I1.text((x + first_width, y), second,
                     fill=part.second_color, font=myFont)
             images.append(img)
--- Main.py
+++ Main.py
@@ -1,8 +1,5 @@
-from PIL import Image, ImageSequence
-from PIL import ImageDraw
-from PIL import ImageFont
-import os
 import sys
+from HiThereGIFGenerator import HTGIFGenerator, HTGIFPart
 
 VERSION = "0.0.1"
 
@@ -13,6 +10,7 @@ file_output_path = "out"
 duration = 80
 hold = 15
 max_width = 900
+font = 'font/DINAlternate-Bold.ttf'
 
 print("""
       *******************************************
@@ -20,25 +18,20 @@ print("""
       *******************************************
       """)
 
-
-def check_arguments():
-    # check for arguments
-    for i in range(len(sys.argv)):
-        if (sys.argv[i] == "-v" or sys.argv[i] == "-version"):
-            print("Version: " + VERSION)
-            quit()
-        if (sys.argv[i] == "-d" or sys.argv[i] == "-duration"):
-            duration = int(sys.argv[i+1])
-            print("Duration: " + str(duration))
-        if (sys.argv[i] == "-h" or sys.argv[i] == "-hold"):
-            hold = int(sys.argv[i+1])
-            print("Hold: " + str(hold) + " frames")
-        if (sys.argv[i] == "-o" or sys.argv[i] == "-output"):
-            file_output_path = sys.argv[i+1]
-            print("Output: " + file_output_path)
-
-
-check_arguments()
+# check for arguments
+for i in range(len(sys.argv)):
+    if (sys.argv[i] == "-v" or sys.argv[i] == "-version"):
+        print("Version: " + VERSION)
+        quit()
+    if (sys.argv[i] == "-d" or sys.argv[i] == "-duration"):
+        duration = int(sys.argv[i+1])
+        print("Duration: " + str(duration))
+    if (sys.argv[i] == "-h" or sys.argv[i] == "-hold"):
+        hold = int(sys.argv[i+1])
+        print("Hold: " + str(hold) + " frames")
+    if (sys.argv[i] == "-o" or sys.argv[i] == "-output"):
+        file_output_path = sys.argv[i+1]
+        print("Output: " + file_output_path)
 
 # get name from user
 name = input("Enter your name: ")
@@ -48,138 +41,22 @@ while (len(name) < 2 or len(name) > 15):
     print("Please enter a name between 2 and 15 characters")
     name = input("Enter your name: ")
 
-print("Generating GIF", end="")
-
-pt = 20
-# set font
-myFont = ImageFont.truetype('font/DINAlternate-Bold.ttf', pt)
-
-img = Image.open(template_file)
-width, height = img.size
-I1 = ImageDraw.Draw(img)
-
-size_width = I1.textlength("I'm " + name, myFont)
-
-# get the maximal font size
-while (size_width < max_width - 30):
-    pt += 1
-    myFont = ImageFont.truetype('font/DINAlternate-Bold.ttf', pt)
-    size_width = I1.textlength("I'm " + name, myFont)
-
-# adjust height
-y = height / 2 - (I1.textbbox((0, 0), "I'm " + name, font=myFont)[3] / 2)
-
-filenames = []
-name_frames = []
-
-# iterate through each character in name
-name_part = ""
-im_part = ""
-counter = 1
-
-# ad "I'm"
-for char in ["I", "'", "m"]:
-    img = Image.open(template_file)
-    im_part += char
-    I1 = ImageDraw.Draw(img)
-
-    # get the center of the image
-
-    letters_width = I1.textlength(im_part, myFont)
-
-    x = width/2 - (letters_width / 2)
-
-    # add name
-    I1.text((x, y), im_part,
-            fill=(0, 181, 175), font=myFont)
-
-    filename = "out/Im"+str(counter)+".png"
-    filenames.append(filename)
-    # Save the edited image
-    # img.save(filename)
-    # name_frames.append(Image.open(filename))
-    name_frames.append(img)
-    counter += 1
-
-print(".", end="")
-
-counter = 1
-
-im_width = I1.textlength("I'm ", myFont)
-
-# add name
-for char in name:
-    img = Image.open(template_file)
-    name_part += char
-    I1 = ImageDraw.Draw(img)
-
-    # get the center of the image
-    width, height = img.size
-
-    letters_width = I1.textlength("I'm " + name_part, myFont)
-    x = width/2 - (letters_width / 2)
-
-    # add "I'm"
-    I1.text((x, y), "I`m",
-            fill=(0, 181, 175), font=myFont)
-
-    # add name
-    I1.text((x + im_width, y), name_part,
-            fill=(51, 54, 149), font=myFont)
-
-    filename = "out/name"+str(counter)+".png"
-    filenames.append(filename)
-    name_frames.append(img)
-    counter += 1
-
-print(".", end="")
-
-name_frames_inverted = name_frames[::-1]
-frames = []
-
-# add start gif
-for frame in ImageSequence.Iterator(Image.open(start_gif)):
-    frame = frame.copy()
-    frames.append(frame)
-
-print(".", end="")
-
-# add name frames
-for frame in name_frames:
-    frames.append(frame)
-
-# hold name for hold frames
-for i in range(0, hold):
-    frames.append(frames[-1])
-
-# add inverted name frames
-for frame in name_frames_inverted:
-    frames.append(frame)
-
-# add end gif
-for frame in ImageSequence.Iterator(Image.open(end_gif)):
-    frame = frame.copy()
-    frames.append(frame)
-
-print(".", end="")
-
 # filepaths
 fp_out = file_output_path+"/HiThereIm"+name+".gif"
 
 # generate gif
-frame_one = frames[0]
-gif = frame_one.save(fp_out, format="GIF", append_images=frames[1:],
-                     save_all=True, duration=duration, loop=0, disposal=2)
+generator = HTGIFGenerator(template_file, fp_out, start_gif, end_gif)
+generator.set_duration(duration)
+print("Duration: " + str(duration))
+generator.set_hold(hold)
+generator.set_max_width(max_width)
 
-print(".")
+hi_part = HTGIFPart(font, "Hi", "There!")
+name_part = HTGIFPart(font, "I'm", name)
 
-
-frames = None
-frames_inverted = None
-
-# delete generated images
-""" for filename in filenames:
-    os.remove(filename) """
+generator.add_part(hi_part)
+generator.add_part(name_part)
+generator.generate()
 
 print("GIF Generated!")
 print("You can find it here: " + fp_out)
